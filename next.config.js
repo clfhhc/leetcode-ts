d8721bf68dc4bf94f85557ca3ccd57594b2bc156
David Chen <david.chen@alliofintech.io>
2022-03-29 20:56:27 -0700

    added github pages settings

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
+const json = require('./package.json');
+
+const projectName = json.name;
+
+const isProduction = process.env.NODE_ENV === 'production'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: isProduction ? `/${projectName}` : '',
 }
 
 module.exports = nextConfig
