a2447d82b02508782af848cfd3b222d01f2b2fdd
David Chen <david.chen@alliofintech.io>
2022-03-29 21:07:15 -0700

    added eslint and prettier

--- next.config.js
+++ next.config.js
@@ -2,12 +2,12 @@ const json = require('./package.json');
 
 const projectName = json.name;
 
-const isProduction = process.env.NODE_ENV === 'production'
+const isProduction = process.env.NODE_ENV === 'production';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   basePath: isProduction ? `/${projectName}` : '',
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
