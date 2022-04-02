62d1a40f96c828fa9664d4069b8a8528799db727
David Chen <david.chen@alliofintech.io>
2022-04-01 21:39:52 -0700

    added keys for leetcode question data

--- lib/urql/getUrqlClientOptions.ts
+++ lib/urql/getUrqlClientOptions.ts
@@ -1,8 +1,9 @@
 import { NextUrqlClientConfig } from 'next-urql';
 import { debugExchange, dedupExchange, fetchExchange } from 'urql';
-import { cacheExchange } from '@urql/exchange-graphcache';
+import { cacheExchange, Data } from '@urql/exchange-graphcache';
 import { getIsClient, getIsProduction } from 'lib/utils/getEnv';
 import { urls } from 'lib/graphql/urls';
+import { Exact, QuestionDataQuery } from 'graphql/leetcode/questionData.query';
 
 export const getUrqlClientOptions: NextUrqlClientConfig = (ssrCache) => {
   const isCLient = getIsClient();
@@ -12,7 +13,18 @@ export const getUrqlClientOptions: NextUrqlClientConfig = (ssrCache) => {
     url: urls['leetcode'],
     exchanges: [
       dedupExchange,
-      cacheExchange({}),
+      cacheExchange({
+        keys: {
+          QuestionNode: (data: Data & QuestionDataQuery['question']) =>
+            data?.questionId ?? null,
+          TopicTagNode: (
+            data: Data &
+              NonNullable<
+                NonNullable<QuestionDataQuery['question']>['topicTags']
+              >[number]
+          ) => data.slug ?? null,
+        },
+      }),
       ...(isCLient && !isProduction ? [debugExchange] : []),
       // ssrExchange has to come after cacheExchange and before fetchExchange
       ssrCache,
