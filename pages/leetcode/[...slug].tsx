055c456b10f77b1dc7b4e0f0602855d8c87f23b2
David Chen <clfhhc@gmail.com>
2022-04-11 17:45:28 -0700

    added forkJoin (#5)
    Co-authored-by: David Chen <david.chen@alliofintech.io>

--- pages/leetcode/[...slug].tsx
+++ pages/leetcode/[...slug].tsx
@@ -5,11 +5,13 @@ import {
 } from 'graphql/leetcode/questionData.query';
 import { getLocalLeetcodeSlugs } from 'lib/leetcode/getLeetcodeFiles';
 import { getUrqlClientOptions } from 'lib/urql/getUrqlClientOptions';
-import { extractFileSection } from 'lib/utils/extractFileSection';
+import { extractFileSectionSource } from 'lib/utils/extractFileSection';
+import { forkJoin } from 'lib/wonka/fokJoin';
 import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
 import { initUrqlClient, withUrqlClient, WithUrqlState } from 'next-urql';
 import { ParsedUrlQuery } from 'querystring';
 import { ssrExchange } from 'urql';
+import { fromValue, map, pipe, toPromise } from 'wonka';
 
 export interface SlugParsedUrlQuery extends ParsedUrlQuery {
   slug: string | string[];
@@ -46,26 +48,31 @@ export const getStaticProps: GetStaticProps<StaticProps> = async ({
   const urqlClientOptions = getUrqlClientOptions(ssrCache);
   const client = initUrqlClient(urqlClientOptions, false);
 
-  const questionRequest = client
-    ?.query(QuestionDataDocument, { titleSlug: slug })
-    .toPromise();
+  const questionSource = client?.query(QuestionDataDocument, {
+    titleSlug: slug,
+  });
 
-  const solutionContentRequest = extractFileSection({
+  const solutionContentSource = extractFileSectionSource({
     filePath: getLocalLeetcodeSlugs()[slug].filePath,
     startPredicate: (line) => line === '/* solution start */',
     endPradicate: (line) => line === '/* solution end */',
   });
 
-  const solutionContent = await solutionContentRequest;
-  const questionData = (await questionRequest)?.data?.question;
+  const result = await pipe(
+    forkJoin([questionSource ?? fromValue(undefined), solutionContentSource]),
+    map(([questionResult, solutionContent]) => {
+      return {
+        props: {
+          solutionContent,
+          questionData: questionResult?.data?.question,
+          urqlState: ssrCache.extractData(),
+        },
+      };
+    }),
+    toPromise
+  );
 
-  return {
-    props: {
-      solutionContent,
-      questionData,
-      urqlState: ssrCache.extractData(),
-    },
-  };
+  return result;
 };
 
 const LeetcodePage: NextPage<StaticProps> = ({
