836149d1eb7cb688a4fb482b9338237c1ee2bf81
David Chen <david.chen@alliofintech.io>
2022-04-21 11:11:44 -0700

    removed unused exportPathMap

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
 const json = require('./package.json');
-// require('@swc/register');
-// const { findLocalLeetcodeFiles } = require('./lib/leetcode/getLeetcodeFiles');
 
 const projectName = json.name;
 
@@ -13,14 +11,6 @@ const nextConfig = {
   basePath: isProduction ? `/${projectName}` : '',
   images: {
     domains: ['github.io'],
-  },
-  exportPathMap: (defaultPathMap, { dir }) => {
-    const pathMap = {
-      '/': { page: '/' },
-      // '/leetcode/*': { page: '/leetcode/[...slug]' },
-    };
-
-    return pathMap;
   },
 };
 
